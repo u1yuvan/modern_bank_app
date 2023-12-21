164792b5e087287118a7af6edf427e44e296d892
u1yuvan <yuvanraj700@gmail.com>
2023-12-21 13:58:20 +0530

    Stats and Business section has been implemented

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { features } from '../constants'
+import styles, { layout } from '../style'
+import Button from './Button'
+
 
 const Business = () => {
   return (
-    <div>Business</div>
+    <section id='features' className={`${layout.section}`}>
+          <div className={layout.sectionInfo}>
+            <h2 className={`${styles.heading2}`}>You do the business, <br className='sm:block hidden'/> 
+            We'll handle the money. </h2>
+            <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Our team of experts uses a methodology to identify the credit cards most likely to fit your needs. We examine annual percentage rates, annual fees.</p>
+          <Button styles="mt-10"/>
+          </div>
+    </section>
   )
 }
 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({ styles }) => {
   return (
-    <div>Button</div>
-  )
+    <button type='button' className={`py-4 px-6 bg-blue-gradient rounded-md font-poppins font-medium text-[18px] text-primary ${styles}`}> Get Started</button>
+    )
 }
 
 export default Button
